e313b78f7acc6db489205efe8dbee3f628c1fee2
jkarout <jkarout@byu.edu>
2025-02-09 16:16:02 -0700

    learning react

---
+++ reactDemo/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  const [bgColor, setBgColor] = React.useState('white');
+
+  const handleClick = () => {
+    setBgColor(bgColor === 'white' ? 'yellow' : 'white');
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      style={{ backgroundColor: bgColor, height: '100vh', font: 'bold 20vh Arial', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
+      <div> Hello React </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
